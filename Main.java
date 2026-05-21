bf0a47375869ebf5a27e50384ee28092aee4f1cb
conofly <a.ruenkov.jr@gmail.com>
2026-05-21 22:14:52 +0300

    Изменен Main.java 3

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Это добавлено в ветке main");
         System.out.println("изменение 1");
+        System.out.println("изменение 2");
+        System.out.println("изменение 3");
     }
 }
