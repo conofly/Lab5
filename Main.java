859504155ecabde60af923cd87b04fd15847bba9
conofly <a.ruenkov.jr@gmail.com>
2026-05-21 22:59:07 +0300

    Изменен MAIN перед слиянием

--- Main.java
+++ Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Это добавлено в ветке main");
-        System.out.println("изменение 1");
-        System.out.println("изменение 2");
-        System.out.println("изменение 3");
-        System.out.println("изменение 4");
+        System.out.println("Изменение Main перед слиянием");
+
     }
 }
