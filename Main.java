d0b2693c1052e4f36d37325dfbb8bd9e6409d30a
conofly <a.ruenkov.jr@gmail.com>
2026-05-21 22:30:58 +0300

    Изменен MAIN в dev 3

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Это добавлено в ветке dev");
         System.out.println("Это изменение ветке dev 1");
+        System.out.println("Это изменение ветке dev 2");
+        System.out.println("Это изменение ветке dev 3");
     }
 }
